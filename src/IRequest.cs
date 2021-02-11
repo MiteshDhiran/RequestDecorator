fa7acb5f5a3a67a7a743011e889b832e904c860e
Mitesh, Dhiran <mitesh_dhiran@hotmail.com>
2021-02-11 16:17:47 +0530

    removed FunctionDecorator from IRequest

--- src/IRequest.cs
+++ src/IRequest.cs
@@ -10,16 +10,8 @@ namespace RequestDecorator
 {
     public interface IRequest<TI, TR, TC>
     {
-        
-        Func<Func<IRequestContext<TI, TR, TC>, Task<Result<TR>>>, Func<IRequestContext<TI, TR, TC>, Task<Result<TR>>>>
-            FunctionDecorator => (Func<IRequestContext<TI, TR, TC>, Task<Result<TR>>> inputFunc) => inputFunc;
-
         TI Data { get; }
-
-
-        
         Func<IRequestContext<TI, TR, TC>, Task<Result<TR>>> ProcessRequestFunc { get; }
-
         Task<TR>  Process(IAPIContext<TC> context);
         
     }
